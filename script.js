1aaa591136ec0c14f1ce50be2c112e91b9697c41
Kirsten Nordine <kirstennordine@gmail.com>
2017-01-03 21:18:52 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,9 @@
+// the following code will make Homer say D'oh! when you click on him!
+$("#profile-picture").click(function () {
+     alert("D'oh!");
+});
+
+
+// now that you know how to make alerts...
+// create an alert when you click on Bart, Ned, and Maggie
+// use the code above as an exampleie
